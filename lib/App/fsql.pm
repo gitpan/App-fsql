8712052866a40ee535f7d1a61a27e92978b7020f
Steven Haryanto <stevenharyanto@gmail.com>
2014-04-17 11:03:45 +0000

    Import of SHARYANTO/App-fsql-0.01 from CPAN.
    gitpan-cpan-distribution: App-fsql
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/App-fsql-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/App/fsql.pm
@@ -0,0 +1,54 @@
+package App::fsql;
+
+our $VERSION = '0.01'; # VERSION
+our $DATE = '2014-04-17'; # DATE
+
+1;
+# ABSTRACT: Perform SQL queries against files in CSV/TSV/LTSV/JSON/YAML formats
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+App::fsql - Perform SQL queries against files in CSV/TSV/LTSV/JSON/YAML formats
+
+=head1 VERSION
+
+version 0.01
+
+=head1 SYNOPSIS
+
+See the command-line script L<fsql>.
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/App-fsql>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/sharyanto/perl-App-fsql>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=App-fsql>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
