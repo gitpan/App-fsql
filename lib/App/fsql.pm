55b1143bbe7c225330713252e2cda4b8e2444f36
Steven Haryanto <stevenharyanto@gmail.com>
2014-04-28 23:36:49 +0000

    Import of SHARYANTO/App-fsql-0.06 from CPAN.
    gitpan-cpan-distribution: App-fsql
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         SHARYANTO/App-fsql-0.06.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/App/fsql.pm
+++ lib/App/fsql.pm
@@ -1,6 +1,6 @@
 package App::fsql;
 
-our $VERSION = '0.05'; # VERSION
+our $VERSION = '0.06'; # VERSION
 our $DATE = '2014-04-29'; # DATE
 
 1;
@@ -18,7 +18,7 @@ App::fsql - Perform SQL queries against files in CSV/TSV/LTSV/JSON/YAML formats
 
 =head1 VERSION
 
-This document describes version 0.05 of module App::fsql (in distribution App-fsql), released on 2014-04-29.
+This document describes version 0.06 of App::fsql (from Perl distribution App-fsql), released on 2014-04-29.
 
 =head1 SYNOPSIS
 
